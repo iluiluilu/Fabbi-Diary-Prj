ef3e225730a881a4489aee0ab96f88b1abd4e287
MinhDT <minhdt.vietnam@gmail.com>
2020-08-10 15:10:41 +0700

    create migration 2

---
+++ database/migrations/2020_08_10_073318_create_diaries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diaries', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('title');
+            $table->string('content');
+            $table->integer('access_range')->nullable();
+            $table->integer('reaction_like')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diaries');
+    }
+}
